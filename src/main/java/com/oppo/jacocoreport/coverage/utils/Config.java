c4d01ca220c4bc4d01ccb7b0954ef9f1441128b3
80289528 <chenwei2@oppo.com>
2020-07-09 15:59:52 +0800

    切换测试环境

--- src/main/java/com/oppo/jacocoreport/coverage/utils/Config.java
+++ src/main/java/com/oppo/jacocoreport/coverage/utils/Config.java
@@ -7,8 +7,8 @@ public class Config {
     public final  static String GitName = "80289528";
     public final  static String GitPassword = "Zhc_172520";
     public final  static String ResourceName = "testenvironment.yml";
-//    public final static String SEND_COVERAGE_URL = "http://atms-test.itest.adc.com/api/codeCoverage/execution/result";
-    public final static String SEND_COVERAGE_URL = "http://172.17.169.157:25896/execution/result";
-//    public static String CLOUD_URL = "http://test-console.cloud.oppoer.me/baymax-go/api/v1/deploy_history?version=";
-    public static String CLOUD_URL = "http://dev-console.cloud.oppoer.me/baymax-go/api/v1/deploy_history?version=";
+    public final static String SEND_COVERAGE_URL = "http://atms-test.itest.adc.com/api/codeCoverage/execution/result";
+//    public final static String SEND_COVERAGE_URL = "http://172.17.169.157:25896/execution/result";
+    public static String CLOUD_URL = "http://test-console.cloud.oppoer.me/baymax-go/api/v1/deploy_history?version=";
+//    public static String CLOUD_URL = "http://dev-console.cloud.oppoer.me/baymax-go/api/v1/deploy_history?version=";
 }
