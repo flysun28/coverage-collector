0305d76cb89e7f015edcfc92259b7220220c2c0b
80289528 <chenwei2@oppo.com>
2020-07-23 14:58:02 +0800

    切换到测试环境

--- src/main/java/com/oppo/jacocoreport/coverage/utils/Config.java
+++ src/main/java/com/oppo/jacocoreport/coverage/utils/Config.java
@@ -9,9 +9,9 @@ public class Config {
     public final  static String ResourceName = "testenvironment.yml";
     public final static String ReportBasePath = "/app/coveragebackend/fawoknqovs7v";
     public final static String ReportBaseUrl = "http://10.84.24.29:8888/";
-    public final static String SEND_COVERAGE_URL = "http://atms.itest.adc.com/api/codeCoverage/execution/result";
+    public final static String SEND_COVERAGE_URL = "http://atms-test.itest.adc.com/api/codeCoverage/execution/result";
 //    public final static String SEND_COVERAGE_URL = "http://172.17.169.157:25896/execution/result";
     public static String CLOUD_URL = "http://test-console.cloud.oppoer.me/baymax-go/api/v1/deploy_history?version=";
 //    public static String CLOUD_URL = "http://dev-console.cloud.oppoer.me/baymax-go/api/v1/deploy_history?version=";
-    public static String SEND_ERRORMESSAGE_URL = "http://atms.itest.adc.com/api/codeCoverage/execution/failed";
+    public static String SEND_ERRORMESSAGE_URL = "http://atms-test.itest.adc.com/api/codeCoverage/execution/failed";
 }
