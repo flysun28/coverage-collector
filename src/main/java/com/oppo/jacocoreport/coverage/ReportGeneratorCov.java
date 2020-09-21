0b6c12cd54233146bcc158d589525b51134d8116
80289528 <chenwei2@oppo.com>
2020-09-21 10:42:26 +0800

    获取覆盖率增加时间戳

--- src/main/java/com/oppo/jacocoreport/coverage/ReportGeneratorCov.java
+++ src/main/java/com/oppo/jacocoreport/coverage/ReportGeneratorCov.java
@@ -317,10 +317,10 @@ public class ReportGeneratorCov {
                             int ipindex = 0;
                             for (String portNum:portList) {
                                 //保持2分覆盖率数据,源代码gitlocalPath工程下存一份
-                                executionDataFile = new File(gitlocalexecutionDataPath, serverip+"_jacoco.exec");//第一步生成的exec的文件
+                                executionDataFile = new File(gitlocalexecutionDataPath, serverip+System.currentTimeMillis()+"_jacoco.exec");//第一步生成的exec的文件
                                 executionDataClient.getExecutionData(serverip, Integer.valueOf(portNum), executionDataFile);
                                 //保存到taskID目录下再存一份
-                                executionDataFile = new File(coverageexecutionDataPath, serverip+"_jacoco.exec");//第一步生成的exec的文件
+                                executionDataFile = new File(coverageexecutionDataPath, serverip+System.currentTimeMillis()+"_jacoco.exec");//第一步生成的exec的文件
                                 boolean getedexecdata = executionDataClient.getExecutionData(serverip, Integer.valueOf(portNum), executionDataFile);
                                 //如果取得覆盖率数据，判断是否有新版本
                                 if (getedexecdata) {
