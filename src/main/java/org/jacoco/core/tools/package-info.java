01ac2f0f2371e6a17250952b4be93ea9e023fba3
80289528 <chenwei2@oppo.com>
2020-10-23 17:59:38 +0800

    导入jacoco源代码，方便二次开发

---
+++ src/main/java/org/jacoco/core/tools/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * <p>
+ * Contributors:
+ * Marc R. Hoffmann - initial API and implementation
+ * <p>
+ * <p>
+ * Collection of tools build on top of the JaCoCo core APIs. The tools offer
+ * more high-level functionality useful for integrating JaCoCo.
+ */
+
+/**
+ * Collection of tools build on top of the JaCoCo core APIs. The tools offer
+ * more high-level functionality useful for integrating JaCoCo.
+ */
+package org.jacoco.core.tools;
