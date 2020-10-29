354fcf985e3f71681aa22ca0dc45f78a701263c5
80289528 <chenwei2@oppo.com>
2020-10-29 10:46:31 +0800

    调整为每天1点05分，开启定时任务

--- src/main/java/com/oppo/jacocoreport/controller/DeleteCoverageTaskScheduled.java
+++ src/main/java/com/oppo/jacocoreport/controller/DeleteCoverageTaskScheduled.java
@@ -10,7 +10,7 @@ import java.io.File;
 
 @Component
 public class DeleteCoverageTaskScheduled {
-    @Scheduled(cron="* 5 * * * ?")
+    @Scheduled(cron="* 5 1 * * ?")
     public void scheduledTask(){
         File taskPath = new File(Config.ReportBasePath,"taskID");
         System.out.println("start delete coverage task");
