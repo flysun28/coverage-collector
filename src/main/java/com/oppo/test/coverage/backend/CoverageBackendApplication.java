f566e03d2c6510602a5dbb1f49e93cbdee2a2a1f
lgx <lingaoxiang@oppo.com>
2021-04-16 11:23:24 +0800

    fix:轮询恢复

--- src/main/java/com/oppo/test/coverage/backend/CoverageBackendApplication.java
+++ src/main/java/com/oppo/test/coverage/backend/CoverageBackendApplication.java
@@ -1,7 +1,13 @@
 package com.oppo.test.coverage.backend;
 
+import com.oppo.test.coverage.backend.util.SystemConfig;
+import com.oppo.test.coverage.backend.util.http.HttpUtils;
+import esa.restlight.spring.util.SpringContextUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Optional;
 
 /**
  * @author :
@@ -10,7 +16,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoverageBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoverageBackendApplication.class, args);
+		ApplicationContext context = SpringApplication.run(CoverageBackendApplication.class, args);
+
+		Optional<SystemConfig> configOptional = SpringContextUtils.getBean(context,SystemConfig.class);
+		configOptional.ifPresent(config -> HttpUtils.sendGet(config.getRecoverTimerTaskUrl()));
 	}
 
 }
