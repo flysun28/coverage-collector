aaa546aa355bc4fd28e9e76996ab1ca3c84233c6
80255355 <123456>
2021-04-16 19:22:36 +0800

    删除无用package

--- src/main/java/com/oppo/jacocoreport/record/service/impl/TestRecordServiceImpl.java
+++ src/main/java/com/oppo/jacocoreport/record/service/impl/TestRecordServiceImpl.java
@@ -40,7 +40,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
