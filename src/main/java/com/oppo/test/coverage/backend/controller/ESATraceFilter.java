8b535d2fd8087af81a0d80810dbfdef13693bb5b
80289528 <chenwei2@oppo.com>
2021-05-13 10:14:30 +0800

    添加调用连配置agent

---
+++ src/main/java/com/oppo/test/coverage/backend/controller/ESATraceFilter.java
@@ -0,0 +1,18 @@
+package com.oppo.test.coverage.backend.controller;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import com.oppo.trace.servlet.TraceFilter;
+
+@Component
+public class ESATraceFilter {
+    @Bean
+    public FilterRegistrationBean traceFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new TraceFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("traceFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+}
