fdafb516e81fa58e998d2d948ebfb4997837945e
80289528 <chenwei2@oppo.com>
2021-06-18 17:56:11 +0800

    修改为sdk获取调用链

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 env_path=$(dirname $0)/../../env.sh
-TRACEAGENT1=-javaagent:/home/service/tools/framework-trace-agent-6.2.0-SNAPSHOT.jar=/home/service/tools/trace-agent.properties
+source $(dirname $0)/agent.sh
+#TRACEAGENT1=-javaagent:/home/service/tools/framework-trace-agent-6.2.0-SNAPSHOT.jar=/home/service/tools/trace-agent.properties
 if [ -f $env_path ]; then
     source $env_path
 fi
@@ -65,7 +66,7 @@ fi
 JAVA_OPTS=" -Djava.io.tmpdir=$base/tmp -DappName=${appName} -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 $JAVA_OPTS"
 if [ -n "$HEY_JVM_OPTIONS" ]; then
     java $REMOTE_JAVA_DEBUG_OPTS -Djava.io.tmpdir=$base/tmp -DappName=${appName} -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 $HEY_JVM_OPTIONS $JAVA_DEBUG_OPT -classpath 'conf:lib/*:.'  com.oppo.test.coverage.backend.CoverageBackendApplication 1>>logs/server.log 2>&1 &
-    else java  $TRACEAGENT1 $JAVA_OPTS $JAVA_DEBUG_OPT -classpath 'conf:lib/*:.'  com.oppo.test.coverage.backend.CoverageBackendApplication 1>>logs/server.log 2>&1 &
+    else java $traceAgent $JAVA_OPTS $JAVA_DEBUG_OPT -classpath 'conf:lib/*:.'  com.oppo.test.coverage.backend.CoverageBackendApplication 1>>logs/server.log 2>&1 &
 fi
 
 echo $! > $base/server.pid
