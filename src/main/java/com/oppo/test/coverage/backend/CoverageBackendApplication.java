3159b862d1452aedf84b9ba0f949578ef6e6fa14
80289528 <chenwei2@oppo.com>
2021-06-18 15:00:48 +0800

    配置bean扫描

--- src/main/java/com/oppo/test/coverage/backend/CoverageBackendApplication.java
+++ src/main/java/com/oppo/test/coverage/backend/CoverageBackendApplication.java
@@ -3,9 +3,12 @@ package com.oppo.test.coverage.backend;
 import com.oppo.test.coverage.backend.util.SystemConfig;
 import com.oppo.test.coverage.backend.util.http.HttpUtils;
 import esa.restlight.spring.util.SpringContextUtils;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Optional;
 
@@ -13,6 +16,9 @@ import java.util.Optional;
  * @author :
  */
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@MapperScan(basePackages = "com.oppo.test.coverage.backend.record.mapper")
 public class CoverageBackendApplication {
 
 	public static void main(String[] args) {
