40414b180e64d0eb8fa3254533640ebab11d019a
80289528 <chenwei2@oppo.com>
2021-06-22 17:56:23 +0800

    修改调用链配置

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -66,7 +66,7 @@ fi
 JAVA_OPTS=" -Djava.io.tmpdir=$base/tmp -DappName=${appName} -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 $JAVA_OPTS"
 if [ -n "$HEY_JVM_OPTIONS" ]; then
     java $REMOTE_JAVA_DEBUG_OPTS -Djava.io.tmpdir=$base/tmp -DappName=${appName} -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 $HEY_JVM_OPTIONS $JAVA_DEBUG_OPT -classpath 'conf:lib/*:.'  com.oppo.test.coverage.backend.CoverageBackendApplication 1>>logs/server.log 2>&1 &
-    else java $JAVA_OPTS $JAVA_DEBUG_OPT -classpath 'conf:lib/*:.'  com.oppo.test.coverage.backend.CoverageBackendApplication 1>>logs/server.log 2>&1 &
+    else java $traceAgent $JAVA_OPTS $JAVA_DEBUG_OPT -classpath 'conf:lib/*:.'  com.oppo.test.coverage.backend.CoverageBackendApplication 1>>logs/server.log 2>&1 &
 fi
 
 echo $! > $base/server.pid
