a73fec2acf8fe830b3d5756dfd5ee2fb51cf69ab
W9008323 <v-yezhipeng@oppo.com>
2024-04-28 09:51:48 +0800

    【提交类型】:新增、修改 【上传人员】:叶志鹏 【修改内容】:优化采集端线程池使用，升级 heracles-client 版本，添加日志 【相关单号】:#22083168 【需要测试】:是

--- src/main/java/com/oppo/test/coverage/backend/CoverageBackendApplication.java
+++ src/main/java/com/oppo/test/coverage/backend/CoverageBackendApplication.java
@@ -4,6 +4,8 @@ import com.oppo.test.coverage.backend.util.SystemConfig;
 import com.oppo.test.coverage.backend.util.http.HttpUtils;
 import esa.restlight.spring.util.SpringContextUtils;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,11 +19,14 @@ import java.util.Optional;
 @MapperScan(basePackages = "com.oppo.test.coverage.backend.record.mapper")
 public class CoverageBackendApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CoverageBackendApplication.class);
+
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(CoverageBackendApplication.class, args);
 
 		Optional<SystemConfig> configOptional = SpringContextUtils.getBean(context, SystemConfig.class);
 		configOptional.ifPresent(config -> HttpUtils.sendGet(config.getRecoverTimerTaskUrl()));
+		LOGGER.warn("CoverageBackendApplication start ok");
 	}
 
 }
